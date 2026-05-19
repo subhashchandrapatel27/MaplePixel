98393698bfc2322f24b544922cbb7131659e396b
Subhashchandra-patel-27 <105190257+subhashpc27@users.noreply.github.com>
2026-05-20 02:04:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Visiting Website
+
+Scaffolded Vite + React project with empty pages.
+
+## Quick start
+
+```bash
+cd "Visiting page"/visiting-website
+npm install
+npm run dev
+```
+
+Open http://localhost:5173
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import SubhashProfile from './SubhashPatel_Profile'
+export default function App() {
+  return (
+    <div>
+        <SubhashProfile></SubhashProfile>
+    </div>
+  )
+}
